fb2a5a5626869c984935554ea03c6bd19255a9e8
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2022-09-25 09:42:24 +0300

    Version variants added.

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,21 @@
+package com.in28minutes.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV1 {
+
+    public String name;
+
+    public PersonV1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV1[" +
+                "name='" + name + '\'' +
+                ']';
+    }
+}
---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonV2.java
@@ -0,0 +1,21 @@
+package com.in28minutes.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV2 {
+
+   private Name name;
+
+   public PersonV2(Name name) {
+      this.name = name;
+   }
+
+   public Name getName() {
+      return name;
+   }
+
+   @Override
+   public String toString() {
+      return "PersonV2[" +
+              "name=" + name +
+              ']';
+   }
+}
