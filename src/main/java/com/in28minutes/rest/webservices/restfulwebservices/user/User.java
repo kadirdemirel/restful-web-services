72292ad6cb74bceb48a8984f216f3118d873ed04
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2022-09-30 15:11:39 +0300

    H2 database configuration settings have been made.

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -2,12 +2,18 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "Name should have atleast 2 characters")
     @JsonProperty("user_name")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(id,birth_date,name)
+values(10001,current_date (),'Ranga');
+
+insert into user_details(id,birth_date,name)
+values(10002,current_date (),'Ravi');
+
+insert into user_details(id,birth_date,name)
+values(10003,current_date (),'Charlie');
